6107602d17904157fac819531e7cd97696a475d0
ymrabti <47449165+ymrabti@users.noreply.github.com>
2020-01-26 14:31:31 +0100

    android maps x

--- app/src/main/java/com/ymrabti/osmmaps/HereActivity.java
+++ app/src/main/java/com/ymrabti/osmmaps/HereActivity.java
@@ -2,16 +2,20 @@ package com.ymrabti.osmmaps;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.here.sdk.core.GeoCoordinates;
 import com.here.sdk.mapviewlite.MapStyle;
 import com.here.sdk.mapviewlite.MapViewLite;
+import com.ymrabti.osmmaps.permession.PermissionsRequestor;
 
 import timber.log.Timber;
 
 public class HereActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private PermissionsRequestor permissionsRequestor;
     private MapViewLite mapView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +27,33 @@ public class HereActivity extends AppCompatActivity {
         }
         mapView = findViewById(R.id.map_view);
         mapView.onCreate(savedInstanceState);
-        loadMapScene();
+        handleAndroidPermissions();
+    }
+
+    private void handleAndroidPermissions() {
+        permissionsRequestor = new PermissionsRequestor(this);
+        permissionsRequestor.request(new PermissionsRequestor.ResultListener(){
+
+            @Override
+            public void permissionsGranted() {
+                loadMapScene();
+            }
+
+            @Override
+            public void permissionsDenied() {
+                Timber.tag(TAG).e("Permissions denied by user.");
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permissionsRequestor.onRequestPermissionsResult(requestCode, grantResults);
     }
     private void loadMapScene() {
-        // Load a scene from the SDK to render the map with a map style.
         mapView.getMapScene().loadScene(MapStyle.NORMAL_DAY, errorCode -> {
             if (errorCode == null) {
-                mapView.getCamera().setTarget(new GeoCoordinates(52.530932, 13.384915));
+                mapView.getCamera().setTarget(new GeoCoordinates(33.547595, -7.650056));
                 mapView.getCamera().setZoomLevel(14);
             } else {
                 Timber.tag("exc").d("onLoadScene failed: %s", errorCode.toString());
